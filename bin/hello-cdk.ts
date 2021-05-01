224da24820d4768991570cc4524c21ea7647c64b
Duo Pan <pd54007@gmail.com>
2021-05-01 20:33:17 +1000

    initial commit

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -3,4 +3,4 @@ import * as cdk from '@aws-cdk/core';
 import { HelloCdkStack } from '../lib/hello-cdk-stack';
 
 const app = new cdk.App();
-new HelloCdkStack(app, 'HelloCdkStack');
+new HelloCdkStack(app, 'ulanab', {env: { account: '', region: 'ap-southeast-2' }});
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,18 +1,63 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // Retrieve default VPC
+    const vpc = new ec2.Vpc(this, 'xing-vpc', {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ]
     });
 
-    const topic = new sns.Topic(this, 'HelloCdkTopic');
+    // Create a security group for our instance
+    const securityGroup = new ec2.SecurityGroup(
+      this,
+      'xing-instance-1-sg',
+      {
+        vpc: vpc,
+        allowAllOutbound: true,
+        securityGroupName: 'xing-instance-1-sg',
+      }
+    );
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allows SSH access from Internet'
+    );
+
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allows HTTP access from Internet'
+    );
+
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'Allows HTTPS access from Internet'
+    );
+
+    const instance = new ec2.Instance(this, 'xing-instance-1', {
+      vpc: vpc,
+      securityGroup: securityGroup,
+      instanceName: 'xing-instance-1',
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+
+      keyName: 'test1',
+    });
   }
 }
